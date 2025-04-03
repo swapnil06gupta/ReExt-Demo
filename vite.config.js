601d49c8db45c8df0ad0b86d28855742dfd04f87
Mohan <mohan.oaktree@gmail.com>
2025-04-03 22:59:51 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import reext from "@sencha/reext/dist/ReExt/vite-plugin-reext.js"; // Use absolute path
 
-import reext from './node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js';
 export default defineConfig({
-  plugins: [
-    
-    reext()
-  ]
-})
+  plugins: [react(), reext()],
+});
